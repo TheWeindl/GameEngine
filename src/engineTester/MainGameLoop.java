64783c25f2daf6c9241797760be5164116eacd1a
TheWeindl <theweindl@gmail.com>
2017-03-01 21:51:25 +0100

    StartUp of the project

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,71 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import entities.Entity;
+import models.RawModel;
+import renderEngine.DisplayManger;
+import renderEngine.Loader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+
+public class MainGameLoop
+{
+
+	public static void main(String[] args)
+	{
+		DisplayManger.createDisplay();
+		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
+		
+		//Triangles
+		float[] vertices = {
+				  -0.5f, 0.5f, 0,
+				  -0.5f, -0.5f, 0,
+				  0.5f, -0.5f, 0,
+				  0.5f, 0.5f, 0f
+		};
+			
+		//Indices
+		int[] indices = {
+				  0,1,3,
+				  3,1,2
+		};
+		
+		//UV coordinates
+		float[] textureCoords = {
+			    0,0,
+			    0,1,
+			    1,1,
+			    1,0
+			  };
+		
+		RawModel model  = loader.loadToVAO(vertices,textureCoords,indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		TexturedModel staticModel = new TexturedModel(model, texture);
+		Entity entity = new Entity(staticModel, new Vector3f(-1,0,0), 0,0,0,1);
+		
+		while(!Display.isCloseRequested()) 
+		{
+			renderer.prepare();
+			
+			//game logic
+			
+			//render
+			shader.start();
+			renderer.render(entity,shader);
+			shader.stop();
+			
+			DisplayManger.updateDisplay();
+		}
+		
+		shader.cleanUp();
+		loader.cleanUP();
+		DisplayManger.closeDisplay();
+	}
+
+}
